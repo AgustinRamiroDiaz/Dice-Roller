5487051e56e2946647aa93be1270b9de9df7f45a
Agustin Diaz <agustin.ramiro.diaz@gmail.com>
2021-09-26 22:54:17 -0300

    Bugfix and better main

--- evaluator.py
+++ evaluator.py
@@ -26,12 +26,19 @@ class Evaluator(Transformer):
     def fudge(self, arguments):
         (quantity, ) = arguments
         rolls = choice([-1, 0, 1], size=quantity)
+
+        mapping_dictionary = {-1: "-", 0: " ", 1: "+"}
+        print(f"Rolling {int(quantity)} fudge dices: {' + '.join(str([mapping_dictionary[key]]) for key in rolls)}")
+
         return rolls.sum()
 
     def dice(self, arguments):
         quantity, number_of_faces = arguments
         # TODO: Extrapolate to negative and floating point numbers
         rolls = choice(int(number_of_faces), size=int(quantity)) + 1
+
+        print(f"Rolling {int(quantity)} {int(number_of_faces)}-sided dices: {' + '.join([str(roll) for roll in rolls])}")
+
         return rolls.sum()
 
     def number(self, arguments):
--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from evaluator import evaluator
 with open("grammar.lark", "r") as file:
     grammar = "\n".join(file.readlines())
 
-parser = Lark(grammar, parser='lalr',
-              start='dice_notation', transformer=evaluator)
+parser = Lark(grammar, start='dice_notation')
 
 # %%
 
 while True:
-    text = input()
-    print(parser.parse(text))
+    text = input("Type in an expression in dice notation: \n")
+    
+    try:
+        tree = parser.parse(text)
+        print(evaluator.transform(tree))
+    except:
+        print("Can't parse expression")
 
-# %%
